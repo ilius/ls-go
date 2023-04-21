a8180421ab9c86d712795cb0ab9e86cfaf4097dc
Saeed Rasooli <saeed.gnu@gmail.com>
2023-04-21 07:22:45 +0330

    fix tests in format/json*

--- format/jsonarray/json_array_test.go
+++ format/jsonarray/json_array_test.go
@@ -5,6 +5,12 @@ import (
 	"github.com/ilius/ls-go/lsargs"
 )
 
+func boolPtr(v bool) *bool {
+	return &v
+}
+
 func init() {
-	var _ iface.Formatter = New(&lsargs.Arguments{})
+	var _ iface.Formatter = New(&lsargs.Arguments{
+		ASCII: boolPtr(false),
+	})
 }
